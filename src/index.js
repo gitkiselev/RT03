248773b7f66e48dc19cd13e5114f02ddea06025f
gitkiselev <482003@mail.ru>
2019-01-25 00:12:28 +0300

    draft

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import App from './App';
+import './app.css';
+import './app-header.css';
+import './post-add-form.css';
+import './post-list.css';
+import './post-list-item.css';
+import './post-status-filter.css';
+import './search-panel.css';
+//import './css/app.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const Header = () => {
+	
+	return <h2>Hello World!</h2>
+}
+
+const Field = () => {
+	const holder = "Enter here";
+	const styledField = {
+		width: '300px'
+	}
+	return <input 
+				style={styledField}	
+				 type="text"
+				 placeholder={holder}
+				 autoComplete=""
+				 className="first"
+				 htmlFor="" />
+}
+
+const Btn = () => {
+	const text = "Log in";
+	const logged = true;
+	const res = () => {
+		return 'Log in, please'
+	}
+	const p = <p>Log in</p>
+	return <button>{logged ? "Enter" : text}</button>
+}
+
+const App = () => {
+	return (
+		<div>
+			<Header/>
+			<Field/>
+			<Btn/>
+		</div>
+		
+		)
+}
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+
