1ecf4a790f6dc1f89110a555bf2bc485035f623d
gitkiselev <482003@mail.ru>
2019-01-25 20:19:08 +0300

    HW03 basic done

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+import '../../app.css';
+import '../../app-header.css';
+import '../../index.css';
+import '../../post-add-form.css';
+import '../../post-list.css';
+import '../../post-list-item.css';
+import '../../post-status-filter.css';
+import '../../search-panel.css';
+
+const App = () => {
+	return (
+			<div className="app">
+				<AppHeader/ >
+				<div className="search-panel d-flex">
+					<SearchPanel />
+					<PostStatusFilter />
+				</div>
+				<PostList/>
+				<PostAddForm />
+			</div>
+		)
+	
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,57 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './components/app/';
+
 import './app.css';
 import './app-header.css';
+import './index.css';
 import './post-add-form.css';
 import './post-list.css';
 import './post-list-item.css';
 import './post-status-filter.css';
 import './search-panel.css';
-//import './css/app.css';
-
-
-
-const Header = () => {
-	
-	return <h2>Hello World!</h2>
-}
-
-const Field = () => {
-	const holder = "Enter here";
-	const styledField = {
-		width: '300px'
-	}
-	return <input 
-				style={styledField}	
-				 type="text"
-				 placeholder={holder}
-				 autoComplete=""
-				 className="first"
-				 htmlFor="" />
-}
-
-const Btn = () => {
-	const text = "Log in";
-	const logged = true;
-	const res = () => {
-		return 'Log in, please'
-	}
-	const p = <p>Log in</p>
-	return <button>{logged ? "Enter" : text}</button>
-}
-
-const App = () => {
-	return (
-		<div>
-			<Header/>
-			<Field/>
-			<Btn/>
-		</div>
-		
-		)
-}
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 
